0321115694cc0e3370c22edea8cb2e720a300b29
Bebestella <stebebenanje@yahoo.com>
2021-12-23 07:47:11 +0530

    adding the backend files

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,4 +1,4 @@
-"""backend URL Configuration
+"""budget_notebook URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', include('apps.posts.urls')),
+    path('transactions/', include('apps.transactions.urls')),
+    path('users/', include('apps.users.urls')),
+    path('categories/', include('apps.categories.urls'))
 ]
