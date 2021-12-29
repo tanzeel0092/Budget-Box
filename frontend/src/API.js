d4ee9beeee1ba1e70746fcd8f952e2d56515b08a
tanzeel0092 <tanzeel.rahman25@outlook.com>
2021-12-28 20:10:54 -0800

    Final update

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -9,7 +9,7 @@ if (
 ) {
   baseURL = process.env.REACT_APP_API_BASE_URL;
 } else {
-  baseURL = "http://127.0.0.1:8000/";
+  baseURL = "https://backend-budget-box.herokuapp.com/";
 }
 
 const api = axios.create({
